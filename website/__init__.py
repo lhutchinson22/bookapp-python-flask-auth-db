50724b3e93c3392aa10edd6d01517ed7bc8aea5b
Lauren Hutchinson <lbhutchinson022@gmail.com>
2025-02-07 16:57:17 -0800

    routes added

--- website/__init__.py
+++ website/__init__.py
@@ -2,5 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "hashdbnclubflubdacrglu"
+    app.config['SECRET'] = "ls"
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
