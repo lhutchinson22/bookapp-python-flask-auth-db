97a8f0021e96f21bbf50d6969316676ef79dd12c
Lauren Hutchinson <lbhutchinson022@gmail.com>
2025-02-22 14:18:53 -0800

    sign up and login in forms, next step will be adding in the javascript for the post requests

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET'] = "ls"
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html', boolean=True)
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +12,8 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template('sign_up.html')
+
+@auth.route('/home')
+def home():
+    return render_template('home.html')
